e023e5115da6826956584cc129224811c4bc26f0
Cassandra Torske <cassandra.torske@gmail.com>
2017-04-04 14:42:08 -0600

    unstyle but all done

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-$(documenent).ready(()=> {
+$(document).ready(function() {
+  console.log("ready");
 
 var answerArray = ['Definitely!', 'Absolutely.', 'Do anything but that.', 'NEVER', 'Affirmative', 'fo shizzle', 'Totally!', 'Might as well', 'Certainly Not', 'NO WAY', 'I do not have an answer at this time.', 'Nope']
 
 var min = Math.ceil(0)
 var max = Math.floor(11)
 
-function randomIndex() {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
+// function randomIndex() {
+//   return Math.floor(Math.random() * (max - min + 1)) + min)
+// }
 
   $('#thebutton').click(function() {
-    $('#answer').append(answerArray[randomIndex()])
+    $('#answer').empty()
+    $('#answer').append(answerArray[(Math.floor(Math.random() * (max - min + 1)) + min)])
   })
 })
