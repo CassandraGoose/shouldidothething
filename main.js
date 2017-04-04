4e485d2419dd6df055895c31cba3f467cf65edbb
Cassandra Torske <cassandra.torske@gmail.com>
2017-04-04 14:35:15 -0600

    basic up

---
+++ main.js
@@ -0,0 +1,15 @@
+$(documenent).ready(()=> {
+
+var answerArray = ['Definitely!', 'Absolutely.', 'Do anything but that.', 'NEVER', 'Affirmative', 'fo shizzle', 'Totally!', 'Might as well', 'Certainly Not', 'NO WAY', 'I do not have an answer at this time.', 'Nope']
+
+var min = Math.ceil(0)
+var max = Math.floor(11)
+
+function randomIndex() {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+  $('#thebutton').click(function() {
+    $('#answer').append(answerArray[randomIndex()])
+  })
+})
